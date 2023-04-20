ade0424d86d677f8fe51f772037f1d69948b70f4
Ririhanna <112195420+Ririhanna@users.noreply.github.com>
2023-04-20 12:09:59 -0400

    update returnLargeNumbersinArray

--- returnLargeNumbersinArray/solution_4.js
+++
@@ -1,7 +0,0 @@
-//Recursive approach
-
-function largestOfFour(arr, finalArr = []) {
-    return !arr.length
-        ? finalArr
-        : largestOfFour(arr.slice(1), finalArr.concat(Math.max(...arr[0])))
-}
